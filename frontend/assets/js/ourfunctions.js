5ba33100182962545d738a3a660d632776cbeb22
kenneth <kmbuthia47@gmail.com>
2017-06-10 00:37:03 +0100

    Fatigue is real!

--- frontend/assets/js/ourfunctions.js
+++ frontend/assets/js/ourfunctions.js
@@ -1,5 +1,5 @@
 function sign_up(fname, lname, known, address, age, gender, phone, email, kin_name, kin_relationship, kin_phone){
-	//var data = "email="+username+"&pass="+password;
+	var data = "fname="+fname+"&lname="+lname+"&known="+known+"&address="+address+"&age="+age+"&gender="+gender+"&phone="+phone+"&email="+email+"&kin_name="+kin_name+"&kin_relationship"+kin_relationship+"&kin_phone"+kin_phone;
 
 	$.ajax({
 	    url : "/forms_wizard",
