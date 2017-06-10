421be6074c00c388e68f0a1163d73adcfcf3c02b
ktertikas <ktertikas@gmail.com>
2017-06-10 07:56:07 +0100

    checkout functionality added

--- frontend/assets/js/checkin.js
+++ frontend/assets/js/checkin.js
@@ -23,3 +23,28 @@ function check_in(){
 	    }
 	});
 }
+
+function check_out(){
+	var email = $("#email").val();
+	console.log(email);
+	var data = "email="+email;
+	console.log(data);
+	$.ajax({
+	    url : "/checkin",
+	    type: "POST",
+	    data : data,
+	    success: function(data, textStatus, jqXHR)
+	    {
+	    	if(data['status']==1){
+	    		alert("SUCCESS!");
+	    	}
+	    	else{
+	    		alert("Error checking in. Please try again.");
+	    	}
+	    },
+	    error: function (jqXHR, textStatus, errorThrown)
+	    {
+	 		//Catch error in case we want to show a popup dialog
+	    }
+	});
+}
