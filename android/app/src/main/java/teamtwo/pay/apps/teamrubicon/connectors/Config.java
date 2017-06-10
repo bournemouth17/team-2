8910f500c4068245c2075cf001d544b0a69a5a7c
kenneth <kmbuthia47@gmail.com>
2017-06-10 05:21:03 +0100

    The entire android app...in one amazing commit! #BestCodeEver

---
+++ android/app/src/main/java/teamtwo/pay/apps/teamrubicon/connectors/Config.java
@@ -0,0 +1,10 @@
+package teamtwo.pay.apps.teamrubicon.connectors;
+
+/**
+ * Created by kenneth on 3/22/16.
+ */
+public class Config {
+
+    public static final String SERVER_URL = "http://188.166.104.20:8000/";
+
+}
