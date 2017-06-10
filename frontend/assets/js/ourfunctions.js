67057cb546bbc447c71c3c2d1df243ce66b1eb8e
ktertikas <ktertikas@gmail.com>
2017-06-10 02:40:06 +0100

    fixed bugs in frontend code

--- frontend/assets/js/ourfunctions.js
+++ frontend/assets/js/ourfunctions.js
@@ -1,5 +1,8 @@
-function sign_up(fname, lname, known, address, age, gender, phone, email, kin_name, kin_relationship, kin_phone){
-	var data = "fname="+fname+"&lname="+lname+"&known="+known+"&address="+address+"&age="+age+"&gender="+gender+"&phone="+phone+"&email="+email+"&kin_name="+kin_name+"&kin_relationship="+kin_relationship+"&kin_phone="+kin_phone;
+function sign_up(fname, lname, known, address, age, gender, phone, email, kin_name, kin_relationship, kin_phone, outdoor, indoor, interests){
+	var data = "fname="+fname+"&lname="+lname+"&known="+known+"&address="+address
+			+"&age="+age+"&gender="+gender+"&phone="+phone+"&email="+email
+			+"&kin_name="+kin_name+"&kin_relationship="+kin_relationship+"&kin_phone="+kin_phone
+			+"&outdoor="+outdoor+"&indoor="+indoor+"&interests"+interests;
 
 	console.log(data);
 
